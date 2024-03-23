7c4d3f2abc8715e5cdf1a1d7896fba1b28cd7ab5
JoaquinFritz16 <162236965+JoaquinFritz16@users.noreply.github.com>
2024-03-23 18:21:24 -0300

    Ultima actualizacion

--- Html1/funcion.js
+++ Html1/funcion.js
@@ -1,15 +1,19 @@
-let fotomostrada="boca-campeon-intercontinental-2000_416x234.webp"
-function myFunction(){
+let fotomostrada = "boca-campeon-intercontinental-2000_416x234.webp";
+
+function myFunction() {
     let imagen = document.getElementById("campeonato");
-    console.log(imagen.src)
-    if(!(imagen.src=="http://127.0.0.1:5500/boca-campeon-intercontinental-2000_416x234.webp")){
-        imagen.src="boca-campeon-intercontinental-2000_416x234.webp";
-        console.log("gwa")
-    }else{
-        imagen.src = "http://127.0.0.1:5500/bianchi.jpg";
-    }
-    if(imagen.src=="http://127.0.0.1:5500/bianchi.jpg"){
-        console.log("gjieog")       
-    }
+    console.log(imagen.src);
+
     
+
+    if (imagen.src.endsWith(fotomostrada)) {
+        imagen.src = "bianchi.jpg";
+        console.log("gwa");
+    } else {
+        imagen.src = fotomostrada;
+    }
+
+    if (imagen.src.endsWith("bianchi.jpg")) {
+        console.log("gjieog");
+    }
 }
